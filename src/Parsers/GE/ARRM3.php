f67d064689679946e2084f9453223a214e85704d
IcarusTwine <36118925+IcarusTwine@users.noreply.github.com>
2021-06-16 18:20:28 +0100

    added first modal for info

--- src/Parsers/GE/ARRM3.php
+++ src/Parsers/GE/ARRM3.php
@@ -3143,6 +3143,7 @@ $ColorStateEnum[3] = "ColorStateReset";
                 
                 <div class=\"w3-bar header-shadow\">
                 <a href=\"../../index.html\" class=\"w3-bar-item w3-button w3-mobile w3-green\">Home</a>
+                <button onclick=\"document.getElementById('id01').style.display='block'\" class=\"w3-button w3-black\">Open Modal</button>
                 <span class=\"w3-bar-item w3-wide\"><b>$MapNameUrl</b></span>
                 </div>
                 
@@ -3670,6 +3671,16 @@ $ColorStateEnum[3] = "ColorStateReset";
                     
                 window.arrmMap = map;
                 </script>
+                <div id=\"id01\" class=\"w3-modal\">
+                    <div class=\"w3-modal-content\">
+                      <div class=\"w3-container\">
+                        <span onclick=\"document.getElementById('id01').style.display='none'\"
+                        class=\"w3-button w3-display-topright\">&times;</span>
+                        <p>Some text in the Modal..</p>
+                        <p>Some text in the Modal..</p>
+                      </div>
+                    </div>
+                  </div>
                 </body>
                 </html>
                 ";
