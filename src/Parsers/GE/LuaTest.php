da72e5880b92c65d8bc978c4f8e6a3d319e85adc
IcarusTwine <36118925+IcarusTwine@users.noreply.github.com>
2021-11-08 17:02:39 +0000

    Monster + Scene

--- src/Parsers/GE/LuaTest.php
+++ src/Parsers/GE/LuaTest.php
@@ -30,48 +30,66 @@ class LuaTest implements ParseInterface
 
         $ENpcBaseCsv = $this->csv("ENpcBase");
         $ENpcResidentCsv = $this->csv("ENpcResident");
-        $CustomTalkCsv = $this->csv("CustomTalk");
-        foreach ($ENpcBaseCsv->data as $id => $ENpcBase) {
-            //if ($id != 1000063) continue; //moogle mail
-            if ($id != 1027385) continue;
-            $ArgArray["BaseId"] = $id;
-            $Name = $ENpcResidentCsv->at($id)['Singular'];
-            foreach(range(0,31) as $i) {
-                if (empty($ENpcBase["ENpcData[$i]"])) continue;
-                if (($ENpcBase["ENpcData[$i]"] > 720000) && ($ENpcBase["ENpcData[$i]"] < 729999)){
-                    $CustomTalkID = $ENpcBase["ENpcData[$i]"];
-                    var_dump($CustomTalkID);
-                    $LuaName = $CustomTalkCsv->at($CustomTalkID)['Name'];
-                    var_dump($LuaName);
-                    $MainOption = $CustomTalkCsv->at($CustomTalkID)['MainOption'];
-                    foreach(range(0,29) as $a) {
-                        $Instruction = $CustomTalkCsv->at($CustomTalkID)["Script{Instruction}[$a]"];
-                        if (empty($Instruction)) continue;
-                        $Argument = $CustomTalkCsv->at($CustomTalkID)["Script{Arg}[$a]"];
-                        $ArgArray[$Instruction] = $Argument; 
+        $QuestCsv = $this->csv("Quest");
+
+        $QuestNo = 69304;
+        function LuaTest($LuaName){
+            $LuaFolder = substr(explode('_', $LuaName)[1], 0, 3);
+            $ini = parse_ini_file('src/Parsers/config.ini');
+            $Resources = str_replace("cache","Resources",$ini['Cache']); 
+            $LuaFile = "$Resources/game_script/quest/$LuaFolder/$LuaName.lua"; 
+            $outarray = [];
+            $CsvTextArray = [];
+            $Code = explode("\n",file_get_contents($LuaFile));
+            file_put_contents('luadump.json', json_encode($Code,JSON_PRETTY_PRINT));
+            $CodeLen = count($Code);
+            $pos = 1;
+            $LastTab = 0;
+            $VariableArray = [];
+            $VariableArray["L0_1"] = "local";
+            while ($pos < $CodeLen) {
+                $Tabs = substr_count($Code[$pos],"  ");
+                $LastTab = $Tabs;
+                $Code[$pos] = str_replace("  ","",$Code[$pos]);
+                if(strpos($Code[$pos],"function") !== false){
+                    $FunctionExp = explode(" ",$Code[$pos]);
+                    $FunctionName = explode("(",$FunctionExp[1])[0];
+                    $CurrentTab = $Tabs;
+
+                    $pos++;
+                    while ($Tabs > $CurrentTab){
+                        
                     }
-                    $Option = "";
-                    if (!empty($MainOption)){
-                        $Option = "=== $MainOption ===";
-                    }
-                $LuaFormat = $this->getLuaDialogue2($LuaName, $ArgArray, $Name, $MainOption);
+
                 }
+
+                sleep(1);
+
+
+
+                $pos++;
             }
         }
-        //var_dump($ArgArray);
+        foreach ($QuestCsv->data as $id => $Quest) {
+            if ($id !== $QuestNo) continue;
+            $LuaPath = $Quest['Id'];
+            $LuaOut = LuaTest($LuaPath);
+
+
+        }
         $console = $console->section();
         
         
         //print_r($LuaFormat);
 
-        $data = GeFormatter::format(self::WIKI_FORMAT, [
-            '{Output}'  => $LuaFormat,
-
-        ]);
-        $this->data[] = $data;
+        //$data = GeFormatter::format(self::WIKI_FORMAT, [
+        //    '{Output}'  => $LuaFormat,
+//
+        //]);
+        //$this->data[] = $data;
         // save
-        $console->writeln(" Saving... ");
-        $info = $this->save("LuaTest.txt", 999999);
+        //$console->writeln(" Saving... ");
+        //$info = $this->save("LuaTest.txt", 999999);
 
     }
 }
--- src/Parsers/Pokemon/Scene_Map.php
+++ src/Parsers/Pokemon/Scene_Map.php
@@ -16,109 +16,203 @@ class Scene_Map implements ParseInterface
     {
         // grab CSV files we want to use
         $Version = $this->getVer();
-        $LanguageMap_en = $this->languagemap("en");
+        $LanguageMap_en = $this->languagemap("English");
 
-        $Scene_Map = $this->json("/$Version/Scene_Map");
-        $PokemonExpGold_Base = $this->json("/$Version/PokemonExpGold_Base");
+        $Scene_Map = $this->json("Scene_Map");
+        $PokemonExpGold_Base = $this->json("PokemonExpGold_Base");
+        $Monster_Base_Monster = $this->json("Monster_Base_Monster");
+        $InherentPropertyDesc = $this->json("InherentPropertyDesc");
+        $ClientTagRaw = $this->json("ClientTag");
+        foreach($ClientTagRaw as $data){
+            $Tag = $data['InGameKey'];
+            $ClientTag[$Tag] = $data;
+        }
+        $Monster_Growth_Monster_Raw = $this->json("Monster_Growth_Monster");
+        foreach($Monster_Growth_Monster_Raw as $data){
+            $Tag = $data['GrowthId'];
+            $Monster_Growth_Monster[$Tag][] = $data;
+        }
+
 
         // (optional) start a progress bar
         $IconArray = [];
         //get evolutions array
 
         // loop through data
+        $GrowthData = [];
         foreach ($Scene_Map as $id => $Scene) {
-            if ($Scene['ShowInList'] === false) continue;
-            $Name = $LanguageMap_en[$Scene['Remarks']];
-            if (empty($Name)) continue;
-            $OutCombatTime = $Scene['OutCombatTime'] / 1000 ."s";
-            $SurrenderCDTime = $Scene['SurrenderCDTime']."s";
-            $DoubleScoreTime = $Scene['DoubleScoreTime'] / 1000 ."s";
-            $BallSkillTable = explode(",",str_replace(array("{","}"),"",$Scene['BallSkillTable']));
-            $TeamAmtPlayers = $Scene['CampPlayerNum'];
-            $DisableQuitGameManually = "false";
-            if (!empty($Scene['DisableQuitGameManually'])){
-                $DisableQuitGameManually = $Scene['DisableQuitGameManually'];
+            if (empty($Scene['ShowInList'])) continue;
+            $SceneFile = $Scene['SceneName'];
+            $MapName = $this->getLangTag($Scene['Remarks'],$LanguageMap_en,$ClientTag);
+            $JSON = file_get_contents("E:\Users\user\Desktop\FF14 Wiki GE\Pokemon Unite Versions/$Version\output/Scenes/$SceneFile.json");
+            $Scene_JSON = json_decode($JSON, true);
+            $Mobs = [];
+            foreach($Scene_JSON["EntitySysCompSerializer"]["SerializedEntities"] as $Entities){
+                if (empty($Entities['SpawnerComp'])) continue;
+                if (empty($Entities['SpawnerComp']['MGroupInfos'])) continue;
+                foreach($Entities['SpawnerComp']['MGroupInfos'] as $MGroupInfos){
+                    if (empty($MGroupInfos['MEntities'])) continue;
+                    foreach($MGroupInfos['MEntities'] as $MEntities){
+                        $MobID = $MEntities['MConfigId'];
+                        if (empty($Monster_Base_Monster[$MobID])) continue; // skip berries etc
+                        if (empty($Monster_Base_Monster[$MobID]['NameCn'])) continue; // skip no names
+                        $MobName = $this->getLangTag($Monster_Base_Monster[$MobID]['NameCn'],$LanguageMap_en,$ClientTag);
+                        $Mobs[$MobName] = $MobID;
+                    }
+                }
             }
-            $MapIcon = $Scene['MapIcon'];
-            $IconArray[] = $MapIcon;
-            $ScoreMinimapDisplay = $Scene['ScoreMinimapDisplay'];
-            $MaxLevel = $Scene['MaxLevel'];
-            $PickTimeOut = $Scene['PickTimeOut'];
-            $HeldDisplay = $Scene['HeldDisplay'];
-            $RouteDisplay = $Scene['RouteDisplay'];
-            $GameMode = $Scene['GameMode'];
-            $SurrenderTimesLimit = $Scene['SurrenderTimesLimit'];
-            $MatchTime = $Scene['MatchTime'] / 1000 ."s";
-            $MonsterEvolutionTime = $Scene['MonsterEvolutionTime'] / 1000 ."s";
-            $ReviveTimeGrowthByTimeExp = explode("},{",$Scene["ReviveTimeGrowthByTime"]);
-            $ReviveTimeGrowth = [];
-            //foreach($ReviveTimeGrowthByTimeExp as $i => $ReviveTimeGrowthByTime){
-            //    $ReviveTimeGrowthByTime = explode(",",str_replace(array("{","}"),"",$ReviveTimeGrowthByTime));
-            //    if (!empty($ReviveTimeGrowthByTime[0])) {
-            //        $ReviveTimeGrowth[] = "|ReviveTimeGrowth_Time_$i = ".$ReviveTimeGrowthByTime[0];
-            //        $ReviveTimeGrowth[] = "|ReviveTimeGrowth_Level_$i = ".$ReviveTimeGrowthByTime[1];
-            //    }
-            //}
-            $ReviveTimeGrowthOut = implode($ReviveTimeGrowth);
-            $MapSubMode = $Scene["MapSubMode"];
-            $ExpGoldId = $Scene['ExpGoldId'];
-            $ExpGold = $PokemonExpGold_Base[$ExpGoldId];
-            $StatTable = [];
-            $StatTable[] = "{{-start-}}";
-            $StatTable[] = "'''$Name/Growth''";
-            $StatTable[] = "{| class=\"wikitable\"";
-            $StatTable[] = "!Level!!LvlUpExp!!BaseKillExp!!ReviveTimeByLevel!!StorageExp!!StorageExpConversion";
+            $Mobs = array_unique($Mobs);
+            $MobOut = [];
+            foreach($Mobs as $MobName => $MobID){
+                $MobData = $Monster_Base_Monster[$MobID];
+                $TitleMobName = $MobName." ($MapName)";
+                $BaseHP = $MobData['BaseHp'];
+                $BaseAttack = $MobData['BaseAttack'];
+                $BaseDef = $MobData['BaseDef'];
+                $BaseMoveSpeed = 0;
+                if (!empty($MobData['BaseMoveSpeed'])){
+                    $BaseMoveSpeed = $MobData['BaseMoveSpeed'] / 1000;
+                }
+                $NormalAttackFrequency = $MobData['NormalAttackFrequency'];
+                $BaseSpecDef = $MobData['BaseSpecDef'];
+                $BaseAbilitySpecialValue = $MobData['BaseAbilitySpecialValue'];
+                $BaseViewableRange = $MobData['BaseViewableRange'] / 1000;
+                $AICallHelpRange = $MobData['AICallHelpRange'] / 1000;
+                $AIVisionRange = $MobData['AIVisionRange'] / 1000;
+                $AIAttackRange = $MobData['AIAttackRange'] / 1000;
+                $AIChaseTime = $MobData['AIChaseTime'] / 1000;
+                $BodyDisappearTime = $MobData['BodyDisappearTime'] / 1000;
+                $BaseKillExp = $MobData['BaseKillExp']; // need to work on this 
+                $BaseKillGold = $MobData['BaseKillGold']; // need to work on this
+                if (!empty($MobData['GrowthId'])){
+                    $GrowthId = $MobData['GrowthId'];
+                    $StatTable = [];
+                    $StatTable[] = "{{-start-}}";
+                    $StatTable[] = "'''$TitleMobName/Growth'''";
+                    $StatTable[] = "{| class=\"wikitable\"";
+                    $StatTable[] = "! Time !!HP!!HP Recovery!!Attack!!Def!!Sp.Atk!!Sp.Def!!Attack Speed!!Speed!!EXP";
+                    $HP = $BaseHP;
+                    $HPRecovery = 0;
+                    $Attack = $BaseAttack;
+                    $Def = $BaseDef;
+                    $SpAtk = $BaseAbilitySpecialValue;
+                    $SpDef = $BaseSpecDef;
+                    $AtkSpeed = $NormalAttackFrequency;
+                    $Speed = $BaseMoveSpeed * 100;
+                    $SpDef = $BaseSpecDef;
+                    foreach ($Monster_Growth_Monster[$GrowthId] as $Growth){
+                        $Time = $Growth['Time'] / 1000;
+                        if (!empty($Growth['KillExpGrowth'])){
+                            $KillExpGrowth = $Growth['KillExpGrowth'];
+                        } else {
+                            $KillExpGrowth = $BaseKillExp;
+                        }
+                        $StatGrowth = $Growth;
+                        
+                        $PropertyNoDiv = $InherentPropertyDesc[1]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[1]['AllowFloatNum'])){
+                        $HP = floor($HP + $StatGrowth['Property'][0] / $PropertyNoDiv);
+                        } else {
+                            $HP = $HP + $StatGrowth['Property'][0] / $PropertyNoDiv;
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[2]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[2]['AllowFloatNum'])){
+                            $HPRecovery = floor($HPRecovery + $StatGrowth['Property'][1] / $PropertyNoDiv);
+                        } else {
+                            $HPRecovery = $HPRecovery + $StatGrowth['Property'][1] / $PropertyNoDiv;
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[3]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[3]['AllowFloatNum'])){
+                            $Attack = floor($Attack + $StatGrowth['Property'][2] / $PropertyNoDiv);
+                        } else {
+                            $Attack = $Attack + $StatGrowth['Property'][2] / $PropertyNoDiv;
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[4]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[4]['AllowFloatNum'])){
+                            $Def = floor($Def + $StatGrowth['Property'][3] / $PropertyNoDiv);
+                        }else {
+                            $Def = $Def + $StatGrowth['Property'][3] / $PropertyNoDiv;
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[5]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[5]['AllowFloatNum'])){
+                            $SpAtk = floor($SpAtk + $StatGrowth['Property'][4] / $PropertyNoDiv);
+                        } else {
+                            $SpAtk = $SpAtk + $StatGrowth['Property'][4] / $PropertyNoDiv;
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[6]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[6]['AllowFloatNum'])){
+                            $SpDef = floor($SpDef + $StatGrowth['Property'][5] / $PropertyNoDiv);
+                        } else {
+                            $SpDef = $SpDef + $StatGrowth['Property'][5] / $PropertyNoDiv;
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[7]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[7]['AllowFloatNum'])){
+                            $AtkSpeed = floor($AtkSpeed + ($StatGrowth['Property'][6] / $PropertyNoDiv));
+                        } else {
+                            $AtkSpeed = $AtkSpeed + ($StatGrowth['Property'][6] / $PropertyNoDiv);
+                        }
+        
+                        $PropertyNoDiv = $InherentPropertyDesc[8]['FormatDivisor'];
+                        if (!empty($InherentPropertyDesc[8]['AllowFloatNum'])){
+                            $Speed = floor($Speed + ($StatGrowth['Property'][7] / $PropertyNoDiv));
+                        } else {
+                            $Speed = $Speed + ($StatGrowth['Property'][7] / $PropertyNoDiv);
+                        }
+        
+                        $AtkSpeed_S = ($NormalAttackFrequency - ($AtkSpeed - 100)) / 100 ."s";
+                        $Speed_M = $Speed / 100 ."m/s";
+                        
+                        $StatTable[] = "|-";
+                        $StatTable[] = "|{$Time}s||$HP||$HPRecovery/s||$Attack||$Def||$SpAtk||$SpDef||".$AtkSpeed / 100 ."% ({$AtkSpeed_S})||$Speed ($Speed_M)||$KillExpGrowth";
+                    }
+                    $StatTable[] = "|}";
+                    $StatTable[] = "{{-stop-}}";
+                    $StatsTables[] = implode("\n",$StatTable);
+                }
 
-            foreach ($ExpGold as $level => $data) {
-                $LvlUpExp = 0;
-                $LevelUpExp = $data['LvlUpExp'];
-                $BaseKillExp = $data['BaseKillExp'];
-                $ReviveTimeByLevel = $data['ReviveTimeByLevel'] / 1000 ."s";
-                $StorageExp = $data['StorageExp'];
-                $StorageExpConversion = $data['StorageExpConversion'] / 100 . "%";
-                $StatTable[] = "|-";
-                $StatTable[] = "|$level||$LevelUpExp||$BaseKillExp||$ReviveTimeByLevel||$StorageExp||$StorageExpConversion";
+
+                $MobOutString = "";
+                $MobOutString .= "'''$TitleMobName'''\n";
+                $MobOutString .= "{{MobTempl\n";
+                $MobOutString .= "|Name = $MobName\n";
+                $MobOutString .= "|Map = $MapName\n";
+                $MobOutString .= "|BaseHP = $BaseHP\n";
+                $MobOutString .= "|BaseAttack = $BaseAttack\n";
+                $MobOutString .= "|BaseAbilitySpecialValue = $BaseAbilitySpecialValue\n";
+                $MobOutString .= "|BaseDef = $BaseDef\n";
+                $MobOutString .= "|BaseMoveSpeed = $BaseMoveSpeed\n";
+                $MobOutString .= "|NormalAttackFrequency = $NormalAttackFrequency\n";
+                $MobOutString .= "|BaseSpecDef = $BaseSpecDef\n";
+                $MobOutString .= "|BaseViewableRange = $BaseViewableRange\n";
+                $MobOutString .= "|AICallHelpRange = $AICallHelpRange\n";
+                $MobOutString .= "|AIVisionRange = $AIVisionRange\n";
+                $MobOutString .= "|AIAttackRange = $AIAttackRange\n";
+                $MobOutString .= "|AIChaseTime = $AIChaseTime\n";
+                $MobOutString .= "|BodyDisappearTime = $BodyDisappearTime\n";
+                $MobOutString .= "|BaseKillExp = $BaseKillExp\n";
+                $MobOutString .= "|BaseKillGold = $BaseKillGold\n";
+                $MobOutString .= "}}\n";
+                $MobOut[] = $MobOutString;
             }
-            $StatTable[] = "|}";
-            $StatTable[] = "{{-stop-}}";
-            $MaxScoreHold = $Scene['LargeAmountScore'];
-            $SurrenderStartTim = $Scene['SurrenderStartTim'];
-            
-            $String = "{{-start-}}\n";
-            $String .= "'''$Name'''\n";
-            $String .= "{{Pokemon Arena\n";
-            $String .= "|Name = $Name\n";
-            $String .= "|MapIcon = $MapIcon\n";
-            $String .= "|MatchTime = $MatchTime\n";
-            $String .= "|TeamPlayers = $TeamAmtPlayers\n";
-            $String .= "|GameMode = $GameMode\n";
-            $String .= "|MapSubMode = $MapSubMode\n";
-            $String .= "|MiniMap = {$Name}_Minimap\n";
-            $String .= "\n";
-            $String .= "|OutCombatTime = $OutCombatTime\n";
-            $String .= "|SurrenderCDTime = $SurrenderCDTime\n";
-            $String .= "|SurrenderStartTime = $SurrenderStartTim\n";
-            $String .= "|SurrenderTimesLimit = $SurrenderTimesLimit\n";
-            $String .= "|DisableQuitGameManually = $DisableQuitGameManually\n";
-            $String .= "|DoubleScoreTime = $DoubleScoreTime\n";
-            $String .= "|MaxScoreHold = $MaxScoreHold\n";
-            $String .= "|PickTimeOut = $PickTimeOut\n";
-            $String .= "|MonsterEvolutionTime = $MonsterEvolutionTime\n";
-            //$String .= "|MonsterEvolutionTime = $ReviveTimeGrowthOut\n";
-            $String .= "\n";
-            $String .= "|ScoreMinimapDisplay = $ScoreMinimapDisplay+\n";
-            $String .= "|MaxLevel = $MaxLevel\n";
-            $String .= "\n";
-            $String .= "}}\n";
-            $String .= "{{-stop-}}\n";
-            $String .= "\n".implode("\n",$StatTable);
-            $Output[] = $String;
+            $MobOutput = implode("\n",$MobOut);
+            $OutString = "";
+            $OutString .= "$MapName\n";
+            $OutString .= "$MobOutput\n";
+
+            $Output[] = $OutString;
         }
-        if (!empty($IconArray)) {
-            $this->copyImages($IconArray,"Scene_Map");
-        }
+        //if (!empty($IconArray)) {
+        //    $this->copyImages($IconArray,"Scene_Map");
+        //}
         // (optional) finish progress bar
-        $this->saveExtra("Output\Scene_Map.txt",implode("\n\n",$Output));
+        $this->saveExtra("Scene_Map.txt",implode("\n\n",$Output));
+        $this->saveExtra("Monster_Growth.txt",implode("\n\n",$StatsTables));
 
         // save
         $this->io->text('Saving data ...');
